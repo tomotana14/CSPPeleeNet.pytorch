c93eb74d76d46dcde6d4bbdaaf7f792ac5dde06c
tomotana14 <tomotana14@gmail.com>
2021-03-03 21:11:43 +0900

    update

--- hubconf.py
+++ hubconf.py
@@ -8,6 +8,9 @@ state_dict_url = 'https://github.com/tomotana14/CSPPeleeNet.pytorch/raw/master/w
 def csppeleenet(pretrained=False, **kwargs):
     net = PeleeNet(partial_ratio=0.5)
     if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(state_dict_url, progress=True)
+        if torch.cuda.is_available() : 
+            state_dict = torch.hub.load_state_dict_from_url(state_dict_url, progress=True)
+        else:
+            state_dict = torch.hub.load_state_dict_from_url(state_dict_url, progress=True, map_location=torch.device('cpu'))
         net.load_state_dict(state_dict)
     return net
